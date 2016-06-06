d8bead698805db3bc02b28fa098e2b0eeeaa5916
Aritra <aritra1704@gmail.com>
2016-06-06 22:36:50 +0530

    Interstitial Ad implementation and adding flavour.

--- app/src/main/java/com/udacity/gradle/builditbigger/Activity/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/Activity/MainActivity.java
@@ -8,12 +8,18 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.arpaul.jokesandroid.ShowJokesActivity;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.udacity.gradle.builditbigger.BuildConfig;
 import com.udacity.gradle.builditbigger.Listener.onJokeReceived;
 import com.udacity.gradle.builditbigger.R;
 import com.udacity.gradle.builditbigger.WebServices.EndpointsAsyncTask;
@@ -22,12 +28,27 @@ import com.udacity.gradle.builditbigger.WebServices.EndpointsAsyncTask;
 public class MainActivity extends AppCompatActivity {
 
     //https://github.com/shivasurya/P4-Build-it-Bigger
+    private InterstitialAd mInterstitialAd;
+    public MaterialDialog materialPB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         MultiDex.install(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                //beginPlayingGame();
+            }
+        });
+
+        requestNewInterstitial();
     }
 
 
@@ -55,6 +76,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view){
 
+        if(BuildConfig.FLAVOR.equals("free")){
+            if (mInterstitialAd.isLoaded()) {
+                mInterstitialAd.show();
+            }
+        } else {
+            showLoader("Please wait..","Loading your jokes..",false);
+        }
+
         new EndpointsAsyncTask().execute(new onJokeReceived() {
             @Override
             public void OnJokeReceivedListener(String joke) {
@@ -63,6 +92,10 @@ public class MainActivity extends AppCompatActivity {
                     /*if (mInterstitialAd.isLoaded()) {
                         mInterstitialAd.show();
                     } else {*/
+                    /*******Closing Interstitial Ad**********/
+                        dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_BACK));
+                        hideLoader();
+
                         Intent intent = new Intent(MainActivity.this,ShowJokesActivity.class);
                         intent.putExtra("jokes",joke);
                         startActivity(intent);
@@ -80,5 +113,88 @@ public class MainActivity extends AppCompatActivity {
         //Toast.makeText(this, new JokesClass().getJokes(), Toast.LENGTH_SHORT).show();
     }
 
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
 
+        mInterstitialAd.loadAd(adRequest);
+    }
+
+    /**
+     * Shows Indefinite Progress Dialog.
+     * @param title
+     * @param message
+     * @param isCancelable
+     */
+    public void showLoader(final String title,final String message, boolean isCancelable){
+        runOnUiThread(new RunProgressDialog(title, message, isCancelable));
+    }
+
+    /**
+     * Shows Indefinite Progress Dialog.
+     * @param title
+     * @param message
+     * @param progress
+     * @param isCancelable
+     */
+    public void showLoader(final String title,final String message, final int progress, boolean isCancelable){
+        runOnUiThread(new RunProgressDialog(title, message, progress, isCancelable));
+    }
+
+    public void hideLoader() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (materialPB != null && materialPB.isShowing())
+                    materialPB.dismiss();
+            }
+        });
+    }
+    class RunProgressDialog implements Runnable {
+        private String strTitle;// Title of the materialDialog
+        private String strMessage;// Message to be shown in materialDialog
+        private boolean isCancelable=false;
+        private int progress;
+
+        public RunProgressDialog(String strTitle,String strMessage, boolean isCancelable)
+        {
+            this.strTitle 		= strTitle;
+            this.strMessage 	= strMessage;
+            this.progress 	    = 0;
+            this.isCancelable 	= isCancelable;
+        }
+
+        public RunProgressDialog(String strTitle,String strMessage, int progress, boolean isCancelable)
+        {
+            this.strTitle 		= strTitle;
+            this.strMessage 	= strMessage;
+            this.progress 	    = progress;
+            this.isCancelable 	= isCancelable;
+        }
+
+        @Override
+        public void run() {
+            if (materialPB != null && materialPB.isShowing())
+                materialPB.dismiss();
+
+            MaterialDialog.Builder builder = new MaterialDialog.Builder(MainActivity.this)
+                    .title(strTitle)
+                    .content(strMessage)
+                    .cancelable(isCancelable);
+
+            builder
+                    .progress(true, progress)
+                    .progressIndeterminateStyle(false);
+
+            try{
+                if (materialPB == null || !materialPB.isShowing()){
+                    materialPB = builder.build();
+                    materialPB.show();
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
